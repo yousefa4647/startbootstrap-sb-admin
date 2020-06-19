3ed3400d0c9387ebfb0d9380a214fa07571ef51b
Yousef Asous <Yousefa4647@hstat.org>
2020-06-19 23:23:56 +0000

    finish

--- dist/assets/demo/chart-bar-demo.js
+++ dist/assets/demo/chart-bar-demo.js
@@ -7,12 +7,12 @@ var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["September", "October", "November", "December", "January", "February", "March", "April", "May", "June"],
     datasets: [{
-      label: "Revenue",
+      label: "Value",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [10000, 9000, 7000 , 9000, 7000, 6000, 10000, 8000, 9000, 9000],
     }],
   },
   options: {
@@ -31,7 +31,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 10000,
           maxTicksLimit: 5
         },
         gridLines: {
